84ee511fdf6233ac75f57dc353e84334ed824597
Larisataranova <110540356+Larisataranova@users.noreply.github.com>
2022-11-20 21:18:36 +0400

    2 модуль
    2 урок готов, БЭМ есть на blog и index ( часть )

---
+++ source/js/index.js
@@ -0,0 +1,12 @@
+var modal = document.querySelector('.modal_window_close-ind')
+var BannerButton = document.querySelector('.banner-ind__button-ind')
+var modalButton = document.querySelector('.modal_button-ind')
+/* ModalWindow */
+
+BannerButton.addEventListener('click', function() {
+    modal.classList.add('modal_window_open-ind')
+}) 
+
+modalButton.addEventListener('click', function() {
+    modal.classList.toggle('modal_window_open-ind')
+})
