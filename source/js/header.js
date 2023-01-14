e97b620c78a756bb0290f99a474fa8f95b9f6aca
Larisataranova <110540356+Larisataranova@users.noreply.github.com>
2023-01-14 13:55:59 +0400

    2 модуль

--- source/js/header.js
+++ source/js/header.js
@@ -1,26 +1,49 @@
-var menuButton = document.querySelector('.menu-hr')
-var cross = document.querySelector('.menu-cross')
-var headerMenu = document.querySelector('.mobile-menu')
-var link = document.querySelector('.link-hr')
+var mobileMenu = document.querySelector('.mobile-menu')
+var mobileLink = document.querySelector('.mobile-menu__link')
+var mobileLinkCS = document.querySelector('.mobile-menu__link-cs')
+var menu = document.querySelector('.menu-hr')
+var mobileCross = document.querySelector('.menu-cross')
 
-/* ModalWindow */
+// ADD
 
-menuButton.addEventListener('click', function() {
-    cross.classList.add('menu-cross-active')
+menu.addEventListener('click', function() {
+    mobileMenu.classList.add('mobile-menu_active')
 })
 
-cross.addEventListener('click', function() {
-    cross.classList.toggle('menu-cross-active')
+menu.addEventListener('click', function() {
+    mobileLink.classList.add('mobile-menu_active__link_active')
 })
 
-cross.addEventListener('click', function() {
-    menuButton.classList.toggle('menu-hr')
+menu.addEventListener('click', function() {
+    mobileLinkCS.classList.add('mobile-menu_active__link-cs_active')
 })
 
-menuButton.addEventListener('click', function() {
-    headerMenu.classList.add('mobile-menu-active')
+menu.addEventListener('click', function() {
+    mobileCross.classList.add('menu-cross_active')
 })
 
-cross.addEventListener('click', function() {
-    headerMenu.classList.toggle('mobile-menu-active')
+menu.addEventListener('click', function() {
+    menu.classList.add('menu-hr_inActiv')
+})
+
+// TOGGLE
+
+mobileCross.addEventListener('click', function() {
+    mobileMenu.classList.toggle('mobile-menu_active')
+})
+
+mobileCross.addEventListener('click', function() {
+    mobileLink.classList.toggle('mobile-menu_active__link_active')
+})
+
+mobileCross.addEventListener('click', function() {
+    mobileLinkCS.classList.toggle('mobile-menu_active__link-cs_active')
+})
+
+mobileCross.addEventListener('click', function() {
+    mobileCross.classList.toggle('menu-cross_active')
+})
+
+mobileCross.addEventListener('click', function() {
+    menu.classList.toggle('menu-hr_inActiv')
 })
